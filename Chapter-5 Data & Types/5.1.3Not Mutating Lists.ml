3731bd61ee711a328564750ed4521646750d39bd
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2024-01-03 23:37:43 +0530

    5.1.4 Pattern Matching with Lists

--- Chapter-5 Data & Types/5.1.3Not Mutating Lists.ml
+++ Chapter-5 Data & Types/5.1.3Not Mutating Lists.ml
@@ -1,6 +1,20 @@
 (* 5.1.3Not Mutating Lists *)
 
+(* Lists are immutable. There’s no way to change an element of a list from one value to another *)
 let inc_first lst =
   match lst with
   | [] -> []
   | h :: t -> h + 1 :: t;;
+
+  (* 5.1.4 Pattern Matching with Lists *)
+  (* Syntax.
+  match e
+  | p1 ->
+  | p2 ->
+  | ...
+  | pn ->
+  with
+  e1
+  e2
+  en *)
+
