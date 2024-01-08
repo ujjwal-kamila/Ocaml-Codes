3bdc13452db7445c35a08af2dc506a30b5af0e69
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2024-01-08 10:44:23 +0530

    Tail Recursion

--- Chapter-5 Data & Types/Lists in Ocaml 5.1.1 to 5.1.9/5.1.9Tail _recursion.ml
+++ Chapter-5 Data & Types/Lists in Ocaml 5.1.1 to 5.1.9/5.1.9Tail _recursion.ml
@@ -2,3 +2,19 @@
 
 (* Def : A function is tail recursive if it calls itself recursively but does not perform any computation after the recursive call returns, and immediately returns to its caller the value of its recursive call *)
 
+  let rec sum (l : int list) : int =
+    match l with
+    | [] -> 0
+    | x :: xs -> x + (sum xs);; (*If the list is not empty, add the first element 'x' to the sum of the rest of the list 'xs' *)
+    sum ([1;2;3]);;
+
+let rec sum_plus_acc (acc : int) (l : int list) : int =
+  match l with
+  | [] -> acc
+  | x :: xs -> sum_plus_acc (acc + x) xs;; (*  (* Recursive case: add current element to accumulator and recurse on the rest of the list *)
+  *)
+  sum_plus_acc 0 [1;2;3;4];;
+
+  let sum_tr : int list -> int =
+    sum_plus_acc 0;;(* Calls a function sum_plus_acc with an initial accumulator value of 0 *)
+    sum_plus_acc 0 [1;2;3;4];;
