6f4034ca0c00cad5de901f5710197f2e99b74393
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2024-01-08 23:41:25 +0530

    Pattern Matching with Lists 5.1.5

--- Chapter-5 Data & Types/Lists in Ocaml 5.1/5.1.5 Deep Pattern Matching.ml
+++ Chapter-5 Data & Types/Lists in Ocaml 5.1/5.1.5 Deep Pattern Matching.ml
@@ -4,6 +4,6 @@
 
 (**_ :: _ *) ....(*matches all lists with at least one element*)
 
-(**_ :: _ ::: []*) ....(*matches all lists with exactly two elements*)
+(**_ :: _ :: []*) ....(*matches all lists with exactly two elements*)
 
 (**_ :: _ :: _ :: _ *) ....(*matches all lists with at least three elements*)
