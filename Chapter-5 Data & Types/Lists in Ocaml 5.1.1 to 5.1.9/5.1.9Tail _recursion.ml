338ebbd0db0385eae9efdba05b0218fea1a8d786
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2024-01-08 10:27:33 +0530

    Tail Recursion

---
+++ Chapter-5 Data & Types/Lists in Ocaml 5.1.1 to 5.1.9/5.1.9Tail _recursion.ml
@@ -0,0 +1,4 @@
+(* 5.1.9. Tail Recursion *)
+
+(* Def : A function is tail recursive if it calls itself recursively but does not perform any computation after the recursive call returns, and immediately returns to its caller the value of its recursive call *)
+
