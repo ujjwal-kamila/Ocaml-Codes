5e781e778e2840d6d4049242798de9f22ad40b08
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2024-01-12 01:05:57 +0530

    5.2. Variants

--- Chapter-5 Data & Types/Varients in Ocaml 5.2/5.2.1varients.ml
+++ Chapter-5 Data & Types/Varients in Ocaml 5.2/5.2.1varients.ml
@@ -2,6 +2,18 @@
 type day = Sun | Mon | Tue | Wed | Thu | Fri | Sat
 let d = Tue;;
 
+let int_of_day d =
+  match d with
+  | Sun -> 1
+  | Mon -> 2
+  | Tue -> 3
+  | Wed -> 4
+  | Thu -> 5
+  | Fri -> 6
+  | Sat -> 7;;
+
+
+
 type t1 = C | D
 type t2 = D | E
 let x = D;;
