78d103ecb83b0f10fb8dc01f6258b758674c712e
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2024-01-13 00:28:54 +0530

    dune project Ounit test

---
+++ Chapter-5 Data & Types/Unit Testing with OUnit 5.3/dune/sum.ml
@@ -0,0 +1,3 @@
+let rec sum = function
+| [] -> 1 (* bug *)
+| x :: xs -> x + sum xs
--- Chapter-5 Data & Types/Unit Testing with OUnit 5.3/ounit.ml
+++ Chapter-5 Data & Types/Unit Testing with OUnit 5.3/ounit.ml
@@ -1,1 +1,10 @@
 (* 5.3.1 An Example of OUnit *)
+(* run below commands  in terminal  *)
+dune build test.exe
+dune exec ./test.exe
+
+(* Now suppose we modify sum.ml to introduce a bug by changing the code in it to the following: *)
+
+(**let rec sum = function
+| [] -> 1 (* bug *)
+| x :: xs -> x + sum xs*)
