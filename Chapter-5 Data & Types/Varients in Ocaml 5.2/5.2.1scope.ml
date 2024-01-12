9881d1cdefd3c76a70aef5f139b6303eec4134ac
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2024-01-12 23:20:32 +0530

    5.2.1 Scope

--- Chapter-5 Data & Types/Varients in Ocaml 5.2/5.2.1scope.ml
+++ Chapter-5 Data & Types/Varients in Ocaml 5.2/5.2.1scope.ml
@@ -1,4 +1,13 @@
 (* 5.2.1 Scope *)
+ (*takes two varients t1 & t2 and store with constructor C | D | E*)
 type t1 = C | D
 type t2 = D | E
-let x = D;;
+let x = D;;  (*assign D to the variable x*)
+(*ans is x : t2 *)
+
+
+
+type ptype =
+TNormal | TFire | TWater
+type peff =
+ENormal | ENotVery | ESuper;;
