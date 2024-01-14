e2aa76febd93be8b9f3b1e665e2ab4ce21c76200
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2024-01-15 00:28:17 +0530

    Function matching 5.5

--- Chapter-5 Data & Types/Advance Pattern Matching 5.5/5.5.1let.ml
+++
@@ -1,1 +0,0 @@
-(* 5.5.1. Pattern Matching with Let *)
---
+++ Chapter-5 Data & Types/Advance Pattern Matching 5.5/5.5.2fun_match.ml
@@ -0,0 +1,8 @@
+(* 5.5.1. Pattern Matching with Let *)
+(*syntax :  "let p = e1 in e2" *)
+
+(* 5.5.2 Pattern Matching with Functions *)
+(**let f p1 ... pn = e1 in e2*)   (* function as part of let expression *)
+(**let f p1 ... pn = e*)          (* function definition at toplevel *)
+(**fun p1 ... pn -> e*)           (* anonymous function *)
+
