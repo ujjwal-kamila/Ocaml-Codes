b4e5d0a63e4667c54da886a30c568c50a86e3e8f
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2024-01-25 23:55:30 +0530

    5.9 Algebraic Data Types

--- Chapter-5 Data & Types/Algebraic Data Types 5.9/5.9algebric.ml
+++ Chapter-5 Data & Types/Algebraic Data Types 5.9/5.9algebric.ml
@@ -4,7 +4,35 @@ type day = Sun | Mon | Tue | Web | Thu | Fri | Sat
 type ptype = TNormal | TFire | Twater 
 type peff = ENormal | ENotVery | Esuper;;
 
-(*5.9.1 Variants that Carry Data*)
+(*From video : 5.9.1 Variants that Carry Data*)
+
+type primary_color = Red | Green | Blue
+let r = Red;;
+let g = Green;;
+type point = float * float
+
+type shape =
+  |Circle of {center : point; radius : float}
+  |Rectangle of {lower_left : point ; upper_right : point}
+
+let c1 =  Circle {center = (0., 0.); radius = 1.}
+let r1 = Rectangle {lower_left = (-1. , -1.); upper_right=(1. , 1.)}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 (* As a running example, here is a variant type shape that does more than just enumerate values: *)
 type point = float * float
 type shape = 
--- Chapter-5 Data & Types/Algebraic Data Types 5.9/tempCodeRunnerFile.ml
+++
@@ -1,4 +0,0 @@
-(* 5.9 Algebraic Data Types *)
-type day = Sun | Mon | Tue | Web | Thu | Fri | Sat 
-type ptype = TNormal | TFire | Twater 
-type peff = ENormal | ENotVery | Esuper;;
