27daa8d91d6ba89f2e02855b205f2d45492469de
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2024-01-28 19:11:20 +0530

    5.9 Algebraic Data Types

--- Chapter-5 Data & Types/Algebraic Data Types 5.9/5.9algebric.ml
+++ Chapter-5 Data & Types/Algebraic Data Types 5.9/5.9algebric.ml
@@ -89,3 +89,17 @@ let center = function
   | Circle (p, _) -> p (* Center of mass of a circle is its center *)
   | Rect ((x1, y1), (x2, y2)) ->(* Center of mass of a rectangle is the midpoint of its diagonal *)
       ((x2 +. x1) /. 2.0, (y2 +. y1) /. 2.0)
+
+(* Here, for example, is a type that represents either a string or an int: *)
+type string_or_int =
+  | String of string
+  | Int of int;;
+
+  type string_or_int_list = string_or_int list
+
+  let rec sum : string_or_int list -> int = function
+    | [] -> 0
+    | String s :: t -> int_of_string s + sum t
+    | Int i :: t -> i + sum t
+  
+  let lst_sum = sum [String "1"; Int 2];;
