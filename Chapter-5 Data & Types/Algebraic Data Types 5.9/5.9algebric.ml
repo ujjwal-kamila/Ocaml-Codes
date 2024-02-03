d15fe5e0bdafb7c9de689791cdc671382b49e0db
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2024-02-03 23:17:10 +0530

    Recursive Varients

--- Chapter-5 Data & Types/Algebraic Data Types 5.9/5.9algebric.ml
+++ Chapter-5 Data & Types/Algebraic Data Types 5.9/5.9algebric.ml
@@ -127,3 +127,34 @@ let string_of_color = function
 
 string_of_color Blue;;
 string_of_color Green;;(*The problem is the catch-all case in the pattern match inside string_of_color: the final case that uses the wildcard pattern to match anything.*)
+(**better way *)
+(* let string_of_color = function
+| Blue -> "blue"
+| Red -> "red";; *)
+
+(*5.9.4 Recursive Varients*)
+(* Define a type 'intlist' that represents a list of integers *)
+type intlist = Nil | Cons of int * intlist
+(* Create a list 'lst3' containing a single element 3 *)
+let lst3 = Cons (3, Nil) (* similar to 3 :: [] or [3] *)
+
+(* Create a list 'lst123' containing elements 1, 2, and 3 *)
+let lst123 = Cons(1, Cons(2, lst3)) (* similar to [1; 2; 3] *)
+
+(* Define a recursive function 'sum' that calculates the sum of elements in the list *)
+let rec sum1 (l : intlist) : int =
+  match l with
+  | Nil -> 0
+  | Cons (h, t) -> h + sum1 t;;
+sum1 lst123;;
+sum1(Cons(1, Cons(2, Cons(3, Cons(4,Nil)))));;
+
+(* Define a recursive function 'length' that calculates the length of the list *)
+let rec length : intlist -> int = function
+  | Nil -> 0
+  | Cons (_, t) -> 1 + length t
+
+(* Define a function 'empty' that checks if the list is empty *)
+let empty : intlist -> bool = function
+  | Nil -> true
+  | Cons _ -> false;;
