59b22723d0222e4160825e67e4c168209ea9f20a
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2024-02-06 00:07:25 +0530

    Higher_order functions

--- Chapter-6 Higher-Order Programming/Higher Order Fun & Map 6.1 & 6.2/high_order.ml
+++ Chapter-6 Higher-Order Programming/Higher Order Fun & Map 6.1 & 6.2/high_order.ml
@@ -1,2 +1,20 @@
 (**A higher-order function is a function that can take other functions as arguments or return functions as results, or bothzA higher-order function is a function that can take other functions as arguments or return functions as results, or both*)
 
+(* Double & square functions *)
+let double x = 2 * x;;
+double 4;;
+let square x = x * x;;
+square 4;;
+
+(*Let’s use these functions to write other functions that quadruple and raise a number to the fourth power: *)
+(* Define a function 'quad' to quadruple a number *)
+let quad x = double (double x);; (*x = x*4 *)
+quad 3;;
+quad 1;;
+
+(* Define a function 'fourth' to raise a number to the fourth power *)
+let fourth x = square (square x);;
+fourth 2;;
+
+(*implement the twice fun from upper*)
+let twice f x = f (f x);;
