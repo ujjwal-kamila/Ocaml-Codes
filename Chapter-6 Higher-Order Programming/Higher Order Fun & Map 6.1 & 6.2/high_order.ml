b9aa20627defe40a5cbe227b0d3971d764802e1a
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2024-02-07 00:39:32 +0530

    Higher order functions

--- Chapter-6 Higher-Order Programming/Higher Order Fun & Map 6.1 & 6.2/high_order.ml
+++ Chapter-6 Higher-Order Programming/Higher Order Fun & Map 6.1 & 6.2/high_order.ml
@@ -18,3 +18,21 @@ fourth 2;;
 
 (*implement the twice fun from upper*)
 let twice f x = f (f x);;
+twice quad 1;;  (*quad 1= 4 .... twice 4 = 4*4 *)
+twice square 2;; (*ans:16,as squares 2 twice: 2*2*2*2.*)
+
+(* quad computes the quadruple of its input by using twice with double. *)
+let quad x = twice double x;;
+quad 1;;
+quad 2;;
+(* fourth computes the fourth power of its input by using twice with square. *)
+let fourth x = twice square x;;
+fourth 1;;
+fourth 3;;
+
+
+(* 6.1.1 The Abstraction Principle *)
+(* We can write a function that applies its first input to its second input: *)
+let apply f x = f x
+
+(*using pipe line operator*)
