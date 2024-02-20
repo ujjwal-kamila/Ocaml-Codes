f306b599cd8b6626870dbca77c0777995dcb94a5
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2024-02-20 23:07:02 +0530

    6.3. Filter

--- Chapter-6 Higher-Order Programming/Higher Order Fun & Map 6.1 & 6.2/6.3filter.ml
+++ Chapter-6 Higher-Order Programming/Higher Order Fun & Map 6.1 & 6.2/6.3filter.ml
@@ -19,7 +19,7 @@ let rec odds = function
   | [] -> []
   | h :: t -> if odd h then h :: odds t else odds t
 
-let lst2 = odds [1; 2; 3; 4]
+let lst2 = odds [1; 2; 3; 4 ; 5]
 
 
 (* The 'filter' function takes a predicate 'p' and a list as arguments.
