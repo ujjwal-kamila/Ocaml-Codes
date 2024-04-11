2f8d6f793ee8b1b7c8a070cd9ebcd8c519a0356a
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2024-04-12 02:10:16 +0530

    Ch - 5 exercises answers

--- Chapter-5 Data & Types/Exercises/4.14exercise.ml
+++ Chapter-5 Data & Types/Exercises/4.14exercise.ml
@@ -22,10 +22,33 @@ assert(product list = 120);;
 (* Exercise: patterns [★★★] *)
 let patterns = function 
 |[]-> false
-|h::t -> if h = "bigred" then true else false ;;
+|h::t -> h = "bigred" ;;
 
 let rec  twofour = function 
 |_::_::[] -> true
 |_::_::_::_::[]-> true
 |_->false;;
+
+let rec check = function 
+|[]->false
+|h1::h2::_-> h1=h2
+|_->false;; 
+
+(* Exercise: library [★★★] *)
+let nth_element lst =
+  if(List.length lst) >= 5 then List.nth lst 4 else 0;;
+
+nth_element [1;2;3;45;44;48;50]
+
+let sorting lst = List.rev (List.sort Stdlib.compare lst);;
+
+
+(* Exercise: library test [★★★] *)
+
+(* Ounit is not avilable  *)
+
+(* Exercise: library puzzle [★★★] *)
+
+
+
 
